47761dbc8fe7650544f358fc27ea36332a3e23b8
pyojeyn <jeynpyo@gmail.com>
2022-05-07 20:01:08 +0900

    0506 암호화 로그인 처리, 로그아웃 하고 페이지 이동 처리 완.

--- src/main/java/com/test/demo/config/SecurityConfig.java
+++ src/main/java/com/test/demo/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.test.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,11 +17,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+
+    /*
+     * 스프링 시큐리티 규칙
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().disable()
                 .csrf().disable()
                 .formLogin().disable()
+                .logout().logoutSuccessUrl("/").disable()
                 .headers().frameOptions().disable();
     }
 }
+
+/*
+    0507 로그아웃하고 자동으로 로그인 되는거 해결함.
+    역시 시큐리티 쪽이었음.
+    로그아웃하고나서 어디로 이동할지도 configure()메소드 안에 설정해줘야함
+    .logout().logoutSuccessUrl("/").disable()
+ */
