91767c5fef8029c842277bac3059591b0b37a060
pyojeyn <jeynpyo@gmail.com>
2022-05-07 20:08:17 +0900

    맥에서 푸쉬

--- src/main/java/com/test/demo/controller/UserApiController.java
+++ src/main/java/com/test/demo/controller/UserApiController.java
@@ -27,7 +27,7 @@ import java.util.Map;
 @RestController
 class UserApiController {
 
-    // 0507 내 데탑에서 커밋 푸쉬
+    // 0507 다시 맥에서 푸쉬
     // 로그하기 위해서 , 팩토리 패턴
     private static Logger logger = LoggerFactory.getLogger(UserApiController.class);
 
