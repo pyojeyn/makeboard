97179bb474d285581bb7e9f1186805b5c3eb922c
pyojeyn <jeynpyo@gmail.com>
2022-06-20 20:00:07 +0900

    0620 다중파일 업로드 도전기 1

---
+++ src/main/java/com/test/demo/vo/BoardFile.java
@@ -0,0 +1,25 @@
+package com.test.demo.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.access.annotation.Secured;
+
+@Getter
+@Setter
+public class BoardFile extends Common{
+
+    /** 파일 번호 (PK) */
+    private Long id;
+
+    /** 게시글 번호 (FK) */
+    private Long boardId;
+
+    /** 원본 파일명 */
+    private String originalName;
+
+    /** 저장 파일명 */
+    private String saveName;
+
+    /** 파일 크기 */
+    private long size;
+}
