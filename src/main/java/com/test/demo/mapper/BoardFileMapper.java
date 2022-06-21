44dc883fecfb92f101e994c280e646a5ed811704
pyojeyn <jeynpyo@gmail.com>
2022-06-22 08:47:42 +0900

    0622 다중파일 첨부도전 2 - BoardFileMapper 만들기

---
+++ src/main/java/com/test/demo/mapper/BoardFileMapper.java
@@ -0,0 +1,17 @@
+package com.test.demo.mapper;
+
+import com.test.demo.vo.BoardFile;
+
+import java.util.List;
+
+public interface BoardFileMapper {
+    public int insertAttach(List<BoardFile> attachList);
+
+    public BoardFile selectAttachDetail(Long idx);
+
+    public int deleteAttach(Long boardIdx);
+
+    public List<BoardFile> selectAttachList(Long boardIdx);
+
+    public int selectAttachTotalCount(Long boardIdx);
+}
