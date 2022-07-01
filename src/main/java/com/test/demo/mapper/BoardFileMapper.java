9ea508533c51a7bcd650a9c2fe9c4829b37f8091
pyojeyn <jeynpyo@gmail.com>
2022-07-01 09:26:31 +0900

    0701 BoardFileMapper.java ㅎㅎ

--- src/main/java/com/test/demo/mapper/BoardFileMapper.java
+++ src/main/java/com/test/demo/mapper/BoardFileMapper.java
@@ -1,9 +1,11 @@
 package com.test.demo.mapper;
 
 import com.test.demo.vo.BoardFile;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface BoardFileMapper {
     public int insertAttach(List<BoardFile> attachList);
 
